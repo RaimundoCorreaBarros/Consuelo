1f04c769bb00095a1f9109971556b56ecf0be1b9
Raimundo <correabarros@gmail.com>
2020-05-17 19:09:34 -0400

    navbar cambio color

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,7 +1,11 @@
 $(window).scroll(function() {
     if ($("nav").offset().top > 400) {
         $("nav").addClass("bg-light");
+        $("nav").addClass("navbar-light");
+        $("nav").removeClass("navbar-dark");
     } else {
         $("nav").removeClass("bg-light");
+        $("nav").removeClass("navbar-light");
+        $("nav").addClass("navbar-dark");
     }
 });
